aa85a4ca81c5827fa5387b1500b41da8cc7c63e8
Jaewoong <dixk3458@naver.com>
2023-10-04 12:17:17 +0900

    7.다크모드

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import styles from './Header.module.css';
+import { useDarkMode } from '../../context/DarkModeContext';
+import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs';
 
 export default function Header({ filters, filter, onFilterChange }) {
+  const { darkMode, toggleDarkMode } = useDarkMode();
   return (
     <header className={styles.header}>
+      <span className={styles.icon}>
+        <button
+          className={styles.darkModeBtn}
+          onClick={() => {
+            toggleDarkMode();
+          }}
+        >
+          {!darkMode ? <BsFillSunFill /> : <BsFillMoonFill />}
+        </button>
+      </span>
       <ul className={styles.filters}>
         {filters.map((value, index) => (
           <li key={index}>
